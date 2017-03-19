3b12638926e7737872b0d2eed829e611cf8629e3
Tamás Perlaki <pt1029@gmail.com>
2017-03-19 18:21:25 +0100

    Static website

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 // import express from 'express';
-
 const express = require('express');
 
 const PORT = 8080;
 
 const app = express();
-app.get('/', (req, res, next) => {
-    res.send('Hello World!');
-});
+app.use('/', express.static('public'));
 
 const server = app.listen(PORT, () => {
     console.log("Listening on port: " + PORT);
